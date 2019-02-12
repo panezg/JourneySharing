4ab1949c92beeca0ddc7b3b594cd1099ba14d72f
JIAmengxuan <1092236342@qq.com>
2019-02-12 17:04:27 +0000

    Could Save data

--- app/src/main/java/org/cs7cs3/team7/journeysharing/ProfileFragment.java
+++ app/src/main/java/org/cs7cs3/team7/journeysharing/ProfileFragment.java
@@ -2,6 +2,8 @@ package org.cs7cs3.team7.journeysharing;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,10 +16,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel mViewModel;
+    public static final String SHARE_PREFS = "sharedPrefs";
+    public static final String NAME = "name";
+    public static final String PHONE = "phone";
+    private String name;
+    private String phone;
 
     public static ProfileFragment newInstance() {
         return new ProfileFragment();
@@ -26,14 +35,39 @@ public class ProfileFragment extends Fragment {
     // Create an anonymous implementation of OnClickListener
     private Button.OnClickListener mSaveOnClickListener = new View.OnClickListener() {
         public void onClick(View v) {
+            saveData();
+        }
+    };
+
+    private void saveData() {
         Log.d("myTag", "This is my message from button");
         View userInfoLayout = getView().findViewById(R.id.user_info_include);
         EditText namesEditText = userInfoLayout.findViewById(R.id.names_text);
         EditText phoneEditText = userInfoLayout.findViewById(R.id.phone_number_text);
         Log.d("myTag", "Names: " + namesEditText.getText().toString());
         Log.d("myTag", "Phone: " + phoneEditText.getText().toString());
-        }
-    };
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(SHARE_PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(NAME, namesEditText.getText().toString());
+        editor.putString(PHONE, phoneEditText.getText().toString());
+        editor.apply();
+
+        //Toast.makeText(this, "Profile Saved!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(SHARE_PREFS, Context.MODE_PRIVATE);
+        name = sharedPreferences.getString(NAME, "");
+        phone = sharedPreferences.getString(PHONE, "");
+    }
+
+    private void updateView() {
+        View userInfoLayout = getView().findViewById(R.id.user_info_include);
+        EditText namesEditText = userInfoLayout.findViewById(R.id.names_text);
+        namesEditText.setText(name);
+        EditText phoneEditText = userInfoLayout.findViewById(R.id.phone_number_text);
+        phoneEditText.setText(phone);
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -52,5 +86,7 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         final Button button = getView().findViewById(R.id.save_button);
         button.setOnClickListener(mSaveOnClickListener);
+        loadData();
+        updateView();
     }
 }
